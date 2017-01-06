fe71c4a348985a42d2ac7183609bea08da74f4dd
ckckckc <refala246@gmail.com>
2017-01-06 14:51:23 +0800

    hello, world

---
+++ l1_hello_world/hello-world.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+app.get('/home', (req, res) => {
+  res.end('Hello World!');
+});
+
+app.listen(process.argv[2]);
+
+//SOLUTION
+// var express = require('express')
+// var app = express()
+// app.get('/home', function(req, res) {
+//   res.end('Hello World!')
+// })
+// app.listen(process.argv[2])
