e5e9667cadb2dc66bf6534851364ca12b24f5db5
ckckckc <refala246@gmail.com>
2017-01-22 17:00:42 +0800

    get query string

---
+++ l7_get_query/server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+
+app.get('/search', function(req, res) {
+  var data = JSON.stringify(req.query);
+  res.end(data);
+});
+
+app.listen(process.argv[2] || 8080);
+
+// SOLUTION
+// var express = require('express')
+// var app = express()
+// 
+// app.get('/search', function(req, res){
+//   var query = req.query
+//   res.send(query)
+// })
+// 
+// app.listen(process.argv[2])
